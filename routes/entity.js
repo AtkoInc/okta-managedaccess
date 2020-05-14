02b662355c3a5dbf86d1f3a2ac46599f26aa3486
Andy March <mail@andymarch.co.uk>
2020-05-14 16:59:45 +0100

    Missing agent model reference

--- routes/entity.js
+++ routes/entity.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
+const agentmodel = require('../models/agentmodel')
 
 module.exports = function (){
 
